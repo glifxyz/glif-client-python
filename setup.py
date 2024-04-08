ecedb7b5af2e65ff0afc1311818982e00ba1ab14
vd <raphaelvorias@gmail.com>
2024-04-08 11:22:27 +0200

    bump versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="glif_client",
-    version="0.1.0",
+    version="0.1.1",
     author="Raphael Vorias",
     author_email="raphael@glif.app",
     description="A wrapper for the Glif API",
